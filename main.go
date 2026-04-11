1113783cee3627d5825cde59d749a61145fbd909
Swan Ghost <swanghost1@users.noreply.github.com>
2026-04-11 21:44:51 +0800

    change default host to localhost for personal/local use

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	defaultPort    = 8080
-	defaultHost    = "0.0.0.0"
+	defaultHost    = "127.0.0.1" // changed from 0.0.0.0 - bind to localhost only by default for personal use
 	appName        = "sub2api"
 	appVersion     = "dev"
 )
